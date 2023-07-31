1174dfb68abcb2e4f6d8891777c3073eeec4d66e
fwcd <fwcdmail@gmail.com>
2023-07-31 17:13:19 +0200

    Test unpacking

--- tests/unpack_test.rs
+++ tests/unpack_test.rs
@@ -12,3 +12,40 @@ fn unsigned_ints() {
 fn signed_ints() {
     assert_eq!(i8::unpack::<BigEndian>(&[255]), -1);
 }
+
+#[test]
+fn derived_structs() {
+    #[derive(Size, Unpack, Clone, Copy, Debug, PartialEq, Eq)]
+    #[allow(dead_code)]
+    struct X {
+        x: u8,
+        y: u16,
+    }
+
+    #[derive(Size, Unpack, Clone, Copy, Debug, PartialEq, Eq)]
+    #[allow(dead_code)]
+    struct Y {
+        x0: X,
+        x1: X,
+        x2: bool,
+    }
+
+    #[derive(Size, Unpack, Clone, Copy, Debug, PartialEq, Eq)]
+    struct Tuple(X, Y);
+
+    #[derive(Size, Unpack, Clone, Copy, Debug, PartialEq, Eq)]
+    struct Unit;
+
+    let x0 = X { x: 3, y: 4 };
+    let x1 = X { x: 2, y: 8 };
+    let y = Y { x0, x1, x2: true };
+
+    assert_eq!(X::unpack::<BigEndian>(&[3, 0, 4]), x0);
+    assert_eq!(X::unpack::<LittleEndian>(&[3, 4, 0]), x0);
+    assert_eq!(X::unpack::<BigEndian>(&[2, 0, 8]), x1);
+    assert_eq!(Tuple::unpack::<BigEndian>(&[3, 0, 4, 3, 0, 4, 2, 0, 8, 1]), Tuple(x0, y));
+    assert_eq!(Unit::unpack::<BigEndian>(&[]), Unit);
+    assert_eq!(Unit::unpack::<BigEndian>(&[1, 2]), Unit);
+}
+
+
