1fe24e034cfe6382366de6988cf3b4336c4cf3ca
fwcd <fwcdmail@gmail.com>
2023-07-31 16:04:39 +0200

    Implement and test size deriving

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,16 +1,16 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{DeriveInput, Data, Fields, Ident};
+use syn::{DeriveInput, Data, Fields, Type};
 
 #[proc_macro_derive(Size)]
 pub fn derive_size(input: TokenStream) -> TokenStream {
     let input: DeriveInput = syn::parse(input).expect("Could not parse derive input");
     let name = &input.ident;
 
-    let fields: Vec<Ident> = match input.data {
+    let field_types: Vec<Type> = match input.data {
         Data::Struct(s) => match s.fields {
             Fields::Named(fs) => fs.named.into_iter()
-                .map(|f| f.ident.expect("#[derive(Size)] requires all fields to be named."))
+                .map(|f| f.ty)
                 .collect(),
             Fields::Unnamed(_) => unimplemented!("#[derive(Size)] is not supported for tuple structs yet!"),
             Fields::Unit => Vec::new(),
@@ -19,7 +19,7 @@ pub fn derive_size(input: TokenStream) -> TokenStream {
         Data::Union(_) => unimplemented!("#[derive(Size)] is not supported for unions yet!"),
     };
 
-    let size_expr = fields.into_iter()
+    let size_expr = field_types.into_iter()
         .map(|f| quote! { #f::SIZE })
         .reduce(|e1, e2| quote! { #e1 + #e2 });
 
--- tests/size_test.rs
+++ tests/size_test.rs
@@ -15,3 +15,22 @@ fn signed_ints() {
     assert_eq!(i32::SIZE, 4);
     assert_eq!(i64::SIZE, 8);
 }
+
+#[derive(Size)]
+struct X {
+    x: u8,
+    y: u16,
+}
+
+#[derive(Size)]
+struct Y {
+    x0: X,
+    x1: X,
+    x2: bool,
+}
+
+#[test]
+fn derived_size() {
+    assert_eq!(X::SIZE, 3);
+    assert_eq!(Y::SIZE, 7);
+}
