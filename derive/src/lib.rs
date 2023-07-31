3f92b1df05e20ef0196484b3e17c20f013f71207
fwcd <fwcdmail@gmail.com>
2023-07-31 15:34:22 +0200

    Stub out proc-macros

---
+++ derive/src/lib.rs
@@ -0,0 +1,16 @@
+use proc_macro::TokenStream;
+
+#[proc_macro_derive(Size)]
+pub fn derive_size(input: TokenStream) -> TokenStream {
+    todo!()
+}
+
+#[proc_macro_derive(Pack)]
+pub fn derive_pack(input: TokenStream) -> TokenStream {
+    todo!()
+}
+
+#[proc_macro_derive(Unpack)]
+pub fn derive_unpack(input: TokenStream) -> TokenStream {
+    todo!()
+}
